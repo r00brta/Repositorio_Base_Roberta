eb1c04fda97cff3d728095158a1a042d2f1cca97
Roberta Mikaelly <robertamikaelly1931@gmail.com>
2025-09-15 10:42:51 -0300

    Create print.py

---
+++ Modulo 1/Codigos Basicos/print.py
@@ -0,0 +1,6 @@
+nome = "mika"
+idade = 15  
+print ("meu nome é" ,nome )
+print ("minha idade é" ,idade)
+print ("meu nome é" ,nome, "minha idade é" ,idade)
+print ("meu nome é" ,nome, "e minha idade é" ,idade)
