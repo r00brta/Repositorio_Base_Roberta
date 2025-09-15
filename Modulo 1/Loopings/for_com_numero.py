32926a259536b1775fba2c4add930bc0c20d6065
Roberta Mikaelly <robertamikaelly1931@gmail.com>
2025-09-15 11:07:16 -0300

    Create for_com_numero.py

---
+++ Modulo 1/Loopings/for_com_numero.py
@@ -0,0 +1,7 @@
+numero = int (input (f" Digite um número inteiro positivo: \n ") )
+if numero > 0:
+        for i in range (1, numero +1):
+              print (i)
+else:
+    print (f" Valor inválido! ")
+    
