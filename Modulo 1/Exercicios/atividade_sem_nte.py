92fbe3055f027674073de171c1f77473f3838f1f
Roberta Mikaelly <robertamikaelly1931@gmail.com>
2025-09-15 10:59:15 -0300

    Create atividade_sem_nte.py

---
+++ Modulo 1/Exercicios/atividade_sem_nte.py
@@ -0,0 +1,21 @@
+print (f" Olá mundo! ")
+
+numero = float (input (f" Digite um número: ") )
+print (f" O número informado foi: {numero}")
+
+numero1= float (input (f" Digite o primeiro número: ") )
+numero2 = float (input (f" Digite o segundo numero: ") )
+soma = numero1 + numero2
+print (f"a soma dos dois numeros é: {soma}")
+
+nome = (input (" Qual é o seu nome? \n ") )
+nota1 = float (input (f" Digite a primeira nota: ") )
+nota2 = float (input (f" Digite a segunda nota: ") )
+nota3 = float (input (f" Digite a terceira nome: ") )
+nota4 = float (input (f" Digite a quarta nota: ") )
+media = (nota1 + nota2 + nota3 + nota4 /4)
+print (f" A média de {nome} é {media}: ")
+
+metros = float (input (f" Digite a quantidade de metros: ") )
+centimetros = metros * 100
+print (f" {metros} metros é igual a {centimetros} centimetors ")
